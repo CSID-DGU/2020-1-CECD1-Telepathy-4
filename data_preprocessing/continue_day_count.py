c69a95f87331180db571d0506085525f5d0937c2
LUV2KUIT <luv2kuit@dongguk.edu>
2020-07-30 21:58:09 +0900

    Continuous step data extraction
    Store a list of consecutive steps for more than a month in a csv file

--- data_preprocessing/continue_day_count.py
+++ data_preprocessing/continue_day_count.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[98]:
+# In[123]:
 
 
 import csv
 import pandas as pd
 
-csv_file = pd.read_csv('output_efil_day_sample.csv')
+csv_file = pd.read_csv('output_samsung_day_sample.csv')
 
 # csv_file
 # 여기까지 불러오는 거
-
-
-# In[99]:
-
 
 i = 2
 j = 0
@@ -30,10 +26,18 @@ for d in csv_file['date']:
     
 # 여기까지 배열에 날짜만 뽑아서 저장하는 거
 
+csvfile = open('output_continue_samsung.csv', 'w', encoding='utf=8', newline='')  # csv 파일 '쓰기'로 열기
+wr = csv.writer(csvfile)
 
-# In[100]:
+#wr.writerow(['person_id', 'date', 'steps'])  # 해당 데이터에 대한 필드 이름
 
-
+def make_list(arr, start, end):
+    for d in range(start, end):
+        date_list = [arr[d][0], arr[d][1], arr[d][2]]
+        date_text = "-".join(date_list)
+        
+        wr.writerow([arr[d][3], date_text, arr[d][4]]) # id, 날짜, 걸음수
+        
 count = 0 # 며칠 연속인지
 n = 0 # 사람 수
 
@@ -57,7 +61,15 @@ for i in range(2, len(arr) - 1):
             else: # 말일이긴 한데 다음 달이랑 안 이어지면
                 if count >= 28:
                     n = n + 1
-                    #print(i, count)
+                    make_list(arr, i - count, i + 1)
+                    
+                    start_date_list = [arr[i - count][0], arr[i - count][1], arr[i - count][2]]
+                    end_date_list = [arr[i][0], arr[i][1], arr[i][2]]
+                    
+                    full_period = "~".join(["".join(start_date_list), "".join(end_date_list)])
+                    
+                    wr.writerow([arr[i][3], full_period, count + 1]) # id, 날짜, 일수
+                    #print(user, i, count)
                 count = 0
         elif month == 4 or month == 6 or month == 9 or month == 11: # 30일
             if (day == 30) and (nextday == 1) and (month + 1 == nextmonth): # 말일이고 다음 달이랑 이어지는 데이터면
@@ -65,7 +77,15 @@ for i in range(2, len(arr) - 1):
             else:
                 if count >= 28:
                     n = n + 1
-                    #print(i, count)
+                    make_list(arr, i - count, i + 1)
+                    
+                    start_date_list = [arr[i - count][0], arr[i - count][1], arr[i - count][2]]
+                    end_date_list = [arr[i][0], arr[i][1], arr[i][2]]
+                    
+                    full_period = "~".join(["".join(start_date_list), "".join(end_date_list)])
+                    
+                    wr.writerow([arr[i][3], full_period, count + 1]) # id, 날짜, 일수
+                    #print(user, i, count)
                 count = 0
         else: # 2월일 때
             if (day == 28) and (nextday == 1) and (month + 1 == nextmonth): # 말일이고 다음 달이랑 이어지는 데이터면
@@ -73,10 +93,20 @@ for i in range(2, len(arr) - 1):
             else:
                 if count >= 28:
                     n = n + 1
-                    #print(i, count)
+                    make_list(arr, i - count, i + 1)
+                    
+                    start_date_list = [arr[i - count][0], arr[i - count][1], arr[i - count][2]]
+                    end_date_list = [arr[i][0], arr[i][1], arr[i][2]]
+                    
+                    full_period = "~".join(["".join(start_date_list), "".join(end_date_list)])
+                    
+                    wr.writerow([arr[i][3], full_period, count + 1]) # id, 날짜, 일수
+                    #print(user, i, count)
                 count = 0
     else: # 하루 단위로 이어지면
         count = count + 1
         
 print(n)
+
+csvfile.close()
 
