28ef5f077c6ef4eac9ff7ed970601807c1583429
yumin oh <55729131+ym-o@users.noreply.github.com>
2020-10-04 18:41:11 +0900

    hierarchical clustering for all data

---
+++ data_preprocessing/all_data_have_outlier_HC.R
@@ -0,0 +1,47 @@
+#Session에서 work space 변경 후 실행하기
+
+x<-read.csv('all_data_for_HR.csv', header = TRUE, stringsAsFactors = FALSE, na.strings = "")
+names(x)
+x
+
+x$name<-NULL # name column 없애기
+
+#x$days_sat<-NULL
+#x$days_sun<-NULL
+#x$days_ends<-NULL
+#x$var<-NULL
+x
+# 거리 행렬 구하기
+rate_scaled<-scale(x) # 표준화
+rate_scaled
+#di<-dist(rate_scaled)
+#as.matrix(di)[1:221,]
+# 거리 행렬 모델 적용(계층적 군집화)
+#fit1<-hclust(d = dist(rate_scaled), method="average")
+# method의 종류보다는 어떤 의도 하에 분류할 것인지에 따라 설명 변수를 선택하는 것이 중요
+#plot(fit1, hang=-1, cex=1.0)
+
+fit1<-hclust(d = dist(rate_scaled), method="complete")
+plot(fit1, hang=-1, cex=1.0)
+
+fit2<-hclust(d = dist(x), method="complete") # 표준화 안 한게 더 좋음
+plot(fit2, hang=-1, cex=1.0)
+
+library(NbClust)
+nc<-NbClust(rate_scaled, distance = "euclidean",min.nc = 2,max.nc = 15,method = "complete")
+nc<-NbClust(x, distance = "euclidean",min.nc = 2,max.nc = 15,method = "complete")
+
+clusters1<-cutree(fit1, k=12) # 표준화 한거 : 개별로
+table(clusters1)
+clusters1
+
+clusters2<-cutree(fit2, k=11) # 표준화 안한거 : 이게 더 좋음 : 왜인지는 나도 몰라
+table(clusters2)
+clusters2
+
+setwd('C:/Users/oym91/OneDrive/Desktop/graph_all')
+write.csv(clusters2, file="data227_HR_no_scaled_4.csv", row.names = FALSE)
+# no_scaled.csv : days_ends 제거 후 k = 12
+# no_scaled_2.csv : days_ends 포함(모든 변수 포함) 후 k = 10
+# no_scaled_3.csv : days_ends 포함(모든 변수 포함) 후 k = 12
+# no_scaled_4.csv : days_ends 포함(모든 변수 포함) 후 k = 11
