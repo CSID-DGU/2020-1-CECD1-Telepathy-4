00b3ad9c0cc9af8b335a447b98e7dd76d912e6a0
nohyoonko <kny_1011@naver.com>
2020-11-01 21:25:53 +0900

    Modify Profile menu details and Add action to button

--- BetterFit/app/src/main/java/com/example/betterfit/ui/profile/ProfileFragment.java
+++ BetterFit/app/src/main/java/com/example/betterfit/ui/profile/ProfileFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.betterfit.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 public class ProfileFragment extends Fragment {
 
@@ -21,6 +27,23 @@ public class ProfileFragment extends Fragment {
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            for (UserInfo profile : user.getProviderData()) {
+                // Name
+                String name = profile.getDisplayName();
+                EditText profileName = root.findViewById(R.id.user_name);
+                profileName.setText(name);
+            }
+        }
+
+        Button button = (Button) root.findViewById(R.id.profileButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_navigation_home);
+            }
+        });
         return root;
     }
 }
